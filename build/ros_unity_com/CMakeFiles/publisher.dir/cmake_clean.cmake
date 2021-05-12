6cf02641f3a9dbb2dfdd10eaa04a7f955e45a7bd
Rowan Ramsey <blazinram@gmail.com>
2021-05-12 17:26:50 +0200

    changes

---
+++ build/ros_unity_com/CMakeFiles/publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/rramsey18/catkin_ws/devel/lib/ros_unity_com/publisher"
+  "/home/rramsey18/catkin_ws/devel/lib/ros_unity_com/publisher.pdb"
+  "CMakeFiles/publisher.dir/src/position_publisher.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/ros_unity_com/src/position_publisher.cpp
+++ src/ros_unity_com/src/position_publisher.cpp
@@ -1,13 +1,19 @@
+#include "ros/console.h"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "geometry_msgs/Vector3.h"
+#include "std_msgs/UInt8MultiArray.h"
 
 #include <sstream>
 
 
 int count = 0;
 ros::Publisher pub;
+ros::Publisher imagePub;
+
 ros::Subscriber sub;
+ros::Subscriber imageSub;
+
 void sendNewPosition(geometry_msgs::Vector3::Ptr recived)
 {
 	geometry_msgs::Vector3 msg;
@@ -16,8 +22,14 @@ void sendNewPosition(geometry_msgs::Vector3::Ptr recived)
 	msg.x = rand()%100;
 	msg.y = rand()%100;
 	msg.z = rand()%100;
-	ROS_INFO("Sending: X:%f, Y:%f, Z:%f",msg.x,msg.y,msg.z);
+	//ROS_INFO("Sending: X:%f, Y:%f, Z:%f",msg.x,msg.y,msg.z);
 	pub.publish(msg);
+}
+
+void processBuffer(std_msgs::UInt8MultiArray::Ptr image)
+{
+	ROS_INFO("Buffer Size: %lu", sizeof(image->data));
+	imagePub.publish(image);
 }
 
 /**
@@ -63,10 +75,13 @@ int main(int argc, char **argv)
    */
   pub = n.advertise<geometry_msgs::Vector3>("get_position", 1000);
   sub = n.subscribe("send_position", 1000, sendNewPosition);
+  imagePub = n.advertise<std_msgs::UInt8MultiArray>("depth_buffer_return",1000);
+  imageSub = n.subscribe("depth_buffer0",1000,processBuffer);
+
 
   geometry_msgs::Vector3::Ptr message;
   sendNewPosition(message);
-  
+
   ros::spin();
   return 0;
 }
