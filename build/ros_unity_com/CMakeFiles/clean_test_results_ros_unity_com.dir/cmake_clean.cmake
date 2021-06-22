96488d26f110da91702271d7af6aa151fa77280a
Rowan Ramsey <rowanramsey18@gmail.com>
2021-06-22 11:04:23 +0200

    removed devel, and build

--- build/ros_unity_com/CMakeFiles/clean_test_results_ros_unity_com.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_test_results_ros_unity_com"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean_test_results_ros_unity_com.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
